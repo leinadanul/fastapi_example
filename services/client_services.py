57e8a2d8d6032bb62a79db6d3eae4dabd665704b
Daniel Ramirez <danielramire1996@gmail.com>
2023-09-22 14:08:00 -0500

    create_client branch ready to up

--- services/client_services.py
+++ services/client_services.py
@@ -1,10 +1,8 @@
-import psycopg2
 from external.dataBase.database_manager import DatabaseManager
 from models.client import Client
 class ClientService():
     def __init__(self,databaseManager: DatabaseManager):
         self.databaseManager= databaseManager
-        self.connection = psycopg2.connect("dbname=library user=postgres password=toor port=5432 ")
 
 
 
